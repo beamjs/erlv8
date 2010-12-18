897da6aa04403d56b7578860ee9edde57dbb0320
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-18 09:31:00 -0800

    Call Module:init(VM) whenever module is registered.

--- src/erlv8_vm.erl
+++ src/erlv8_vm.erl
@@ -265,7 +265,8 @@ register(Server, Mod) when is_atom(Mod) ->
 	register(Server, Mod, Mod).
 
 register(Server, Name, Mod) when is_atom(Mod) ->
-	register(Server, Name, fun () -> Mod:exports() end);
+	register(Server, Name, fun () -> Mod:exports() end),
+	Mod:init(Server);
 
 register(Server, Name, Mod) when is_function(Mod) ->
 	Global = global(Server),
