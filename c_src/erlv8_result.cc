623cd1314fd414f3f7b0374fe8d3be64e04c751c
Yurii Rashkovskii <yrashk@gmail.com>
2011-01-12 18:31:46 -0800

    No more general reticking

--- c_src/erlv8_result.cc
+++ c_src/erlv8_result.cc
@@ -6,6 +6,20 @@ TickHandler(ResultTickHandler) {
 	result = term_to_js(vm->env,array[2]);
 	return RETURN;
   } else {
-   return NEXT;
+   zmq_msg_t tick_msg;
+   Tick * newtick = (Tick *) malloc(sizeof(Tick));
+   int e;
+   
+   newtick->env = enif_alloc_env();
+   newtick->tick = enif_make_copy(newtick->env, tick);
+   newtick->ref = enif_make_copy(newtick->env, tick_ref);
+   
+   zmq_msg_init_data(&tick_msg, newtick, sizeof(Tick), free_tick, NULL);
+   do {
+	 e = zmq_send(vm->push_socket, &tick_msg, ZMQ_NOBLOCK);
+   } while (e == EAGAIN);
+   zmq_msg_close(&tick_msg);
+   
+   return DONE;
   }
 }
