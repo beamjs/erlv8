5d2028d6ed5e6cb273eda6f57254bcea9a07d9e6
Gleb Peregud <gleber.p@gmail.com>
2012-11-06 20:53:42 +0100

    rewrite erlv8_object to use module tuples calls instead of parametrized modules

--- src/erlv8_object.erl
+++ src/erlv8_object.erl
@@ -1,108 +1,115 @@
--module(erlv8_object,[Resource,VM]).
--export([proplist/0, set_value/2, set_value/3, set_hidden_value/2, get_value/1, get_value/2, get_hidden_value/1, get_hidden_value/2, 
-		 internal_field_count/0, get_internal_field/1, set_internal_field/2,
-		 set_prototype/1, get_prototype/0, delete/1, set_accessor/2, set_accessor/3, set_accessor/4, set_accessor/5,
-		 equals/1, strict_equals/1, call/1, call/2,new/1]).
+-module(erlv8_object).
 
-proplist() ->
-	erlv8_vm:enqueue_tick(VM,{proplist, Resource}).
+-record(erlv8_object, {resource,vm}).
 
-set_value(Key,Value) ->
-	erlv8_vm:enqueue_tick(VM, {set, Resource, Key, Value}).
+-export([proplist/1, set_value/3, set_value/4, set_hidden_value/3, get_value/2, get_value/3, get_hidden_value/2, get_hidden_value/3,
+         internal_field_count/1, get_internal_field/2, set_internal_field/3,
+         set_prototype/2, get_prototype/1, delete/2, set_accessor/3, set_accessor/4, set_accessor/5, set_accessor/6,
+         equals/2, strict_equals/2, call/2, call/3,
 
-set_value(Key,Value,PropertyAttribute) ->
-	erlv8_vm:enqueue_tick(VM, {set, Resource, Key, Value, PropertyAttribute}).
+         new/1, new/2]).
 
-set_hidden_value(Key,Value) ->
-	erlv8_vm:enqueue_tick(VM, {set_hidden, Resource, Key, Value}).
+proplist({_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM,{proplist, Resource}).
 
-get_value(Key) ->
-	get_value(Key, undefined).
+set_value(Key, Value, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {set, Resource, Key, Value}).
 
-get_value(Key, Default) ->
-	case erlv8_vm:enqueue_tick(VM, {get, Resource, Key}) of
-		undefined ->
-			Default;
-		Val ->
-			Val
-	end.
+set_value(Key,Value,PropertyAttribute, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {set, Resource, Key, Value, PropertyAttribute}).
 
-get_hidden_value(Key) ->
-	get_hidden_value(Key, undefined).
+set_hidden_value(Key,Value, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {set_hidden, Resource, Key, Value}).
 
-get_hidden_value(Key, Default) ->
-	case erlv8_vm:enqueue_tick(VM, {get_hidden, Resource, Key}) of
-		undefined ->
-			Default;
-		Val ->
-			Val
-	end.
+get_value(Key, {_Erlv8Obj,  _Resource, _VM} = Self) ->
+    get_value(Key, undefined, Self).
 
-internal_field_count() ->
-	erlv8_vm:enqueue_tick(VM, {internal_count, Resource}).
+get_value(Key, Default, {_Erlv8Obj,  Resource, VM}) ->
+    case erlv8_vm:enqueue_tick(VM, {get, Resource, Key}) of
+        undefined ->
+            Default;
+        Val ->
+            Val
+    end.
 
-get_internal_field(Index) ->
-	erlv8_vm:enqueue_tick(VM, {get_internal, Resource, Index}).
+get_hidden_value(Key, {_Erlv8Obj,  _Resource, _VM} = Self) ->
+    get_hidden_value(Key, undefined, Self).
 
-set_internal_field(Index, {extern, Value}) ->
-	erlv8_vm:enqueue_tick(VM, {set_internal_extern, Resource, Index, Value, erlv8_extern:type(Value)});
+get_hidden_value(Key, Default, {_Erlv8Obj,  Resource, VM}) ->
+    case erlv8_vm:enqueue_tick(VM, {get_hidden, Resource, Key}) of
+        undefined ->
+            Default;
+        Val ->
+            Val
+    end.
 
-set_internal_field(Index, Value) ->
-	erlv8_vm:enqueue_tick(VM, {set_internal, Resource, Index, Value}).
+internal_field_count({_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {internal_count, Resource}).
 
-set_prototype(Proto) ->
+get_internal_field(Index, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {get_internal, Resource, Index}).
+
+set_internal_field(Index, {extern, Value}, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {set_internal_extern, Resource, Index, Value, erlv8_extern:type(Value)});
+
+set_internal_field(Index, Value, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {set_internal, Resource, Index, Value}).
+
+set_prototype(Proto, {_Erlv8Obj,  Resource, VM}) ->
     erlv8_vm:enqueue_tick(VM, {set_proto, Resource, Proto}).
 
-get_prototype() ->
-	erlv8_vm:enqueue_tick(VM, {get_proto, Resource}).
+get_prototype({_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {get_proto, Resource}).
 
-delete(Key) ->
-	erlv8_vm:enqueue_tick(VM, {delete, Resource, Key}).
+delete(Key, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_vm:enqueue_tick(VM, {delete, Resource, Key}).
 
-set_accessor(Property, Getter) ->
-	case erlv8_vm:enqueue_tick(VM, {set_accessor, Resource, Property, Getter}) of
+set_accessor(Property, Getter, {_Erlv8Obj,  Resource, VM}) ->
+    case erlv8_vm:enqueue_tick(VM, {set_accessor, Resource, Property, Getter}) of
         badarg ->
             throw(badarg);
         Result ->
             Result
     end.
 
-set_accessor(Property, Getter, Setter) ->
-	case erlv8_vm:enqueue_tick(VM, {set_accessor, Resource, Property, Getter, Setter}) of
+set_accessor(Property, Getter, Setter, {_Erlv8Obj,  Resource, VM}) ->
+    case erlv8_vm:enqueue_tick(VM, {set_accessor, Resource, Property, Getter, Setter}) of
         badarg ->
             throw(badarg);
         Result ->
             Result
     end.
 
-set_accessor(Property, Getter, Setter, AccessControl) ->
-	case erlv8_vm:enqueue_tick(VM, {set_accessor, Resource, Property, Getter, Setter, AccessControl}) of
+set_accessor(Property, Getter, Setter, AccessControl, {_Erlv8Obj,  Resource, VM}) ->
+    case erlv8_vm:enqueue_tick(VM, {set_accessor, Resource, Property, Getter, Setter, AccessControl}) of
         badarg ->
             throw(badarg);
         Result ->
             Result
     end.
 
-set_accessor(Property, Getter, Setter, AccessControl, PropertyAttribute) ->
-	case erlv8_vm:enqueue_tick(VM, {set_accessor, Resource, Property, Getter, Setter, AccessControl, PropertyAttribute}) of
+set_accessor(Property, Getter, Setter, AccessControl, PropertyAttribute, {_Erlv8Obj,  Resource, VM}) ->
+    case erlv8_vm:enqueue_tick(VM, {set_accessor, Resource, Property, Getter, Setter, AccessControl, PropertyAttribute}) of
         badarg ->
             throw(badarg);
         Result ->
             Result
     end.
 
-equals({_Tag,AnotherObject,_}) ->
-	erlv8_value:equals(VM, Resource, AnotherObject).
+equals({_Tag,AnotherObject,_}, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_value:equals(VM, Resource, AnotherObject).
 
-strict_equals({_Tag,AnotherObject,_}) ->
-	erlv8_value:strict_equals(VM, Resource, AnotherObject).
+strict_equals({_Tag,AnotherObject,_}, {_Erlv8Obj,  Resource, VM}) ->
+    erlv8_value:strict_equals(VM, Resource, AnotherObject).
 
-call(Fun) ->
-	call(Fun,[]).
+call(Fun, Self) ->
+    call(Fun,[], Self).
 
-call(Fun,Args) ->
+call(Fun,Args, {_Erlv8Obj,  Resource, VM}) ->
     Fun:call({erlv8_object, Resource,VM}, Args).
-	
+
 new(O) ->
-	instance(O,undefined).
-	
+    new(O,undefined).
+
+new(O, V) ->
+    #erlv8_object{resource = O, vm = V}.
